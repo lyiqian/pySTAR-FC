b6f58f22121409e02258af14e9bc58274f76ae6b
Eason Liu <yql@yorku.ca>
2024-04-26 10:18:34 -0400

    Disable GPU installation for cluster

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -40,13 +40,13 @@ esac
 done
 
 #Check driver version and update the DRIVER LINK
-DRIVER_VERSION=$(glxinfo | grep "OpenGL version string" | rev | cut -d" " -f1 | rev) 
+# DRIVER_VERSION=$(glxinfo | grep "OpenGL version string" | rev | cut -d" " -f1 | rev) 
 
-DRIVER_LINK="http://us.download.nvidia.com/XFree86/Linux-x86_64/$DRIVER_VERSION/NVIDIA-Linux-x86_64-$DRIVER_VERSION.run"
-if [ ! -f NVIDIA-DRIVER.run ]; then
-    wget $DRIVER_LINK 
-    mv `basename $DRIVER_LINK` NVIDIA-DRIVER.run
-fi
+# DRIVER_LINK="http://us.download.nvidia.com/XFree86/Linux-x86_64/$DRIVER_VERSION/NVIDIA-Linux-x86_64-$DRIVER_VERSION.run"
+# if [ ! -f NVIDIA-DRIVER.run ]; then
+#     wget $DRIVER_LINK 
+#     mv `basename $DRIVER_LINK` NVIDIA-DRIVER.run
+# fi
 
 
 docker buildx build -t ${IMAGE_NAME}:${TAG} \
