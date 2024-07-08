9798a3e88babb87b8b962fcdd92eac0f1f749205
Eason Liu <yql@yorku.ca>
2024-07-08 15:09:57 -0400

    Complete concrete classes for GSV

--- src/wrapper.py
+++ src/wrapper.py
@@ -1,4 +1,8 @@
 import abc
+import os
+
+import fabric
+import invoke
 
 
 # ABCs
@@ -26,6 +30,9 @@ class AbstractStarFC(abc.ABC):
     def calc_fixation(self, image):
         pass
 
+    def connect(self, ssh_conn):
+        self.ssh_conn = ssh_conn
+
     def process_single(self, path=None):
         image = self.img_reader.read(path)
         next_fixation = self.calc_fixation(image)
@@ -33,25 +40,64 @@ class AbstractStarFC(abc.ABC):
 
 
 ## Concrete Classes
+GSV_CONN_STRING = "eason@gsv.eecs.yorku.ca"
+MYPASS = os.getenv("GSV_PW")
+LOCAL_IMG_FILENAME = 'bridge-cards-s.jpg'
+REMOTE_IMG_FILENAME = 'curr_frame.jpg'
+
+
 class GsvStarFC(AbstractStarFC):
+    CONFIG_PATH = 'config_files/pantilt.ini'
     def calc_fixation(self, image):
-        print("TODO Calc next fixation")
+        print("Calc next fixation")
+        cmd = (
+            f'cd ~/repos/pySTAR-FC/docker '
+            f'&& sudo docker exec starfc python3 src/STAR_FC.py -v -c {self.CONFIG_PATH}'
+        )
+        sudopass = invoke.Responder(pattern=r'\[sudo\] password for eason:', response=MYPASS+'\n')
+        run_fc_result = self.ssh_conn.run(cmd, pty=True, watchers=[sudopass])
+
+        if run_fc_result.stderr:
+            print("ERROR")
+            print(run_fc_result.stderr)
 
 
 class SshImageReader(IImageReader):
+    """This acts as a putter."""
+    LOCAL_IMG_DIRPATH = '/home/yiqian/repos/pySTAR-FC/images/'
+    REMOTE_IMG_DIRPATH = '/home/eason/repos/pySTAR-FC/images/'
+
+    def __init__(self, ssh_conn) -> None:
+        self.conn = ssh_conn
+
     def read(self, path=None):
-        print("TODO Reading path", path)
+        local_img_filepath = self.LOCAL_IMG_DIRPATH + path
+        remote_img_filepath = self.REMOTE_IMG_DIRPATH + REMOTE_IMG_FILENAME
+        print("Putting to", remote_img_filepath)
+        self.conn.put(local_img_filepath, remote=remote_img_filepath)
 
 
 class SshFixationWriter(IFixationWriter):
+    """This acts as a getter."""
+    img_name = REMOTE_IMG_FILENAME.rsplit('.', maxsplit=1)[0]
+    REMOTE_OUTPUT_PATH = f'/home/eason/repos/pySTAR-FC/output/{img_name}/fixations_{img_name}.mat'
+    LOCAL_OUTPUT_PATH = f'/home/yiqian/repos/pySTAR-FC/output/from_wrapper/fixations_{img_name}.mat'
+
+    def __init__(self, ssh_conn) -> None:
+        self.conn = ssh_conn
+
     def write(self, fixations):
-        print("TODO Writing fixations")
+        print("Downloading to", self.LOCAL_OUTPUT_PATH)
+        self.conn.get(self.REMOTE_OUTPUT_PATH, local=self.LOCAL_OUTPUT_PATH)
 
 
 if __name__ == '__main__':
     # Example usage
-    gsv_star_fc = GsvStarFC(
-        img_reader=SshImageReader(),
-        fixation_writer=SshFixationWriter())
+    ssh_conn = fabric.Connection(GSV_CONN_STRING, connect_kwargs=dict(password=MYPASS))
 
-    gsv_star_fc.process_single()
+    img_reader = SshImageReader(ssh_conn)
+    fix_writer = SshFixationWriter(ssh_conn)
+    gsv_star_fc = GsvStarFC(img_reader, fix_writer)
+
+    gsv_star_fc.connect(ssh_conn)
+    gsv_star_fc.process_single(LOCAL_IMG_FILENAME)
