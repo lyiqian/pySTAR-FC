0102f3dd09dd3365b2a4c871259ac5f292f936b8
Eason Liu <yichian.liu@gmail.com>
2024-07-31 17:17:00 -0400

    Add more TODOs for embodied stfc

--- src/wrapper.py
+++ src/wrapper.py
@@ -9,26 +9,34 @@ NextFixation = object  # TODO need to think about passing around (e.g. via ssh) 
 
 
 # ABCs
-## YL can write concrete one and only create ABC on 2nd concrete
+## YL workflow: can write concrete one and only create ABC on 2nd concrete
 class IImageReader(abc.ABC):
     @abc.abstractmethod
     def read(self, img_src):
         pass
 
 
-class IFixationWriter(abc.ABC):
+class IFixationLoader(abc.ABC):
     @abc.abstractmethod
-    def write(self, fixation: NextFixation):
+    def load(self, fixation_src):
+        pass
+
+
+class IEyeMover(abc.ABC):
+    @abc.abstractmethod
+    def saccade(self, next_fixation):
         pass
 
 
 class AbstractStarFC(abc.ABC):
     img_reader: IImageReader
-    fixation_writer: IFixationWriter
+    fixation_loader: IFixationLoader
+    eye_mover: IEyeMover
 
-    def __init__(self, img_reader, fixation_writer) -> None:
+    def __init__(self, img_reader, fixation_loader, eye_mover) -> None:
         self.img_reader = img_reader
-        self.fixation_writer = fixation_writer
+        self.fixation_loader = fixation_loader
+        self.eye_mover = eye_mover
 
     @abc.abstractmethod
     def calc_fixation(self, image):
@@ -36,8 +44,9 @@ class AbstractStarFC(abc.ABC):
 
     def process_single(self, img_src=None):
         image = self.img_reader.read(img_src)
-        next_fixation = self.calc_fixation(image)
-        self.fixation_writer.write(next_fixation)
+        fixation_src = self.calc_fixation(image)
+        next_fixation = self.fixation_loader.load(fixation_src)
+        self.eye_mover.saccade(next_fixation)
 
 
 ## Concrete Classes
@@ -54,6 +63,7 @@ class GsvStarFC(AbstractStarFC):
         self.ssh_conn = ssh_conn
 
     def calc_fixation(self, image):
+        # TODO dynamically change config file based on `image`
         print("Calc next fixation")
         cmd = (
             f'cd ~/repos/pySTAR-FC/docker '
@@ -68,7 +78,7 @@ class GsvStarFC(AbstractStarFC):
 
 
 class SshImageReader(IImageReader):
-    """This acts as a putter."""
+    """This acts as a POST."""
     LOCAL_IMG_DIRPATH = '/home/yiqian/repos/pySTAR-FC/images/'
     REMOTE_IMG_DIRPATH = '/home/eason/repos/pySTAR-FC/images/'
 
@@ -82,8 +92,8 @@ class SshImageReader(IImageReader):
         self.conn.put(local_img_filepath, remote=remote_img_filepath)
 
 
-class SshFixationWriter(IFixationWriter):
-    """This acts as a getter."""
+class SshFixationLoader(IFixationLoader):
+    """This acts as a GET."""
     img_name = REMOTE_IMG_FILENAME.rsplit('.', maxsplit=1)[0]
     REMOTE_OUTPUT_PATH = f'/home/eason/repos/pySTAR-FC/output/{img_name}/fixations_{img_name}.mat'
     LOCAL_OUTPUT_PATH = f'/home/yiqian/repos/pySTAR-FC/output/from_wrapper/fixations_{img_name}.mat'
@@ -91,9 +101,10 @@ class SshFixationWriter(IFixationWriter):
     def __init__(self, ssh_conn) -> None:
         self.conn = ssh_conn
 
-    def write(self, fixation: NextFixation):
+    def load(self, fixation_src) -> NextFixation:  # TODO pass in remove output path
         print("Downloading to", self.LOCAL_OUTPUT_PATH)
         self.conn.get(self.REMOTE_OUTPUT_PATH, local=self.LOCAL_OUTPUT_PATH)
+        # TODO return next_fixation
 
 
 if __name__ == '__main__':
@@ -101,8 +112,8 @@ if __name__ == '__main__':
     ssh_conn = fabric.Connection(GSV_CONN_STRING, connect_kwargs=dict(password=MYPASS))
 
     img_reader = SshImageReader(ssh_conn)
-    fix_writer = SshFixationWriter(ssh_conn)
-    gsv_star_fc = GsvStarFC(img_reader, fix_writer)
+    fix_loader = SshFixationLoader(ssh_conn)
+    gsv_star_fc = GsvStarFC(img_reader, fix_loader)
 
     gsv_star_fc.connect(ssh_conn)
     gsv_star_fc.process_single(LOCAL_IMG_FILENAME)
